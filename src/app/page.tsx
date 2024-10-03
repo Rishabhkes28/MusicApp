48b04d616e2386e3c6a9f15609f796eba145b476
“[Rishabh <[rishabhkeshari10@gmail.com]>
2024-10-03 19:26:03 +0530

    completed FeaturedCourses Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image"
 import Hero from "./components/Hero";
+import Featuredcourses from "./components/Featuredcourses";
 
 export default function Home() {
   return (
      <div className="min-h-screen border- bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       
     <Hero/>
+    <Featuredcourses/>
     </div>
   );
 }

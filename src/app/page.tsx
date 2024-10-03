bea421d9ca0a6fb5defb98e21f64b04b01a69b21
“[Rishabh <[rishabhkeshari10@gmail.com]>
2024-10-03 18:04:49 +0530

    completed Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image"
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
      <div className="min-h-screen border- bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center"> Chai aur Code</h1>
+      
+    <Hero/>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,21 @@
 import type { Config } from "tailwindcss";
+// const svgToDataUri = require("mini-svg-data-uri");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+// import { flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
+
+function addVariablesForColors({ addBase, theme }: any) {
+  const allColors = flattenColorPalette(theme('colors'));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, value]) => [`--${key}`, value])
+  );
+
+  addBase({
+    ':root': newVars,
+  });
+}
 
 const config: Config = {
   content: [
@@ -9,12 +26,28 @@ const config: Config = {
   darkMode: "class",
   theme: {
     extend: {
-      colors: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+         colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes:{
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity:" 1",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
+      },
+    
     },
-  },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
 export default config;

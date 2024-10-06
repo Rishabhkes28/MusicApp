8ae68c2a67b87ea0e9307135b550e06adecc3181
“[Rishabh <[rishabhkeshari10@gmail.com]>
2024-10-06 18:09:42 +0530

    Completed UpcomingWebinar Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Featuredcourses from "./components/Featuredcourses";
 import WhyChooseUs from "./components/WhyChooseUs";
 import TestimonialCards from "./components/TestimonialCards";
+import UpcomingWebinar from "./components/UpcomingWebinar";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
     <Featuredcourses/>
     <WhyChooseUs/>
     <TestimonialCards/>
+    <UpcomingWebinar/>
     </div>
   );
 }
